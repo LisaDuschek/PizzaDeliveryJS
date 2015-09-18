a2c9ee806911fc23584f65c05e126e5dc16cf814
Lisa Duschek <lisa.duschek@hotmail.com>
2015-09-18 11:41:38 -0700

    revised frontend plus add jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ function Pizza(quantity, size) {
 
 Pizza.prototype.calculate = function(size, amount, tomatoes, olives, tuna, meat) {
   var price = size * 2 * amount + meat;
-  return price;
+  this.orderPrice.push(this.price);
+  return this.price;
  };
 
 
@@ -22,4 +23,32 @@ Toppings.prototype.addTopping = function(topping, amount) {
 function Ingredient(topping, amount) {
   this.topping = topping;
   this.amount = amount;
+};
+
+
+
+$(document).ready(function() {
+    $("#order").click(function(event) {
+        var size = parseInt($('input[name="size"]:checked').val());
+        var amount = parseInt($('input[name="amount"]:checked').val());
+        var tomatoes = parseInt($('input[name="tomatoes"]:checked').val());
+        var olives = parseInt($('input[name="olives"]:checked').val());
+        var tuna = parseInt($('input[name="tuna"]:checked').val());
+        var meat = parseInt($('input[name="meat"]:checked').val());
+
+        var newOrder = new Pizza(size, amount, tomatoes, olives, tuna, meat);
+      newOrder.calculate(size, amount, tomatoes, olives, tuna, meat);
+
+
+    });
+
+
+    $("#show_order").text("Congratulations for stuffing your face with " + newOrder.amount + " pizzas of ." +
+      newOrder.amount + " " + sizeInsert + " As toppings you chose: "
+      + newOrder.tomatoes + " servings of tomatoes, " + newOrder.olives +
+       " servings of olives and " + newOrder.tuna + " servings of tuna " + newOrder.meat + " servings of meat,you greedy bastard.")
+
+  });
+
+
 };
